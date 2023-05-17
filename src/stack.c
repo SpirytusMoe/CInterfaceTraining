c7a470c566cd304dfa9082c4fc7eb3b5b93851e8
SpirytusMoe <ochyaken@nicobird.xyz>
2023-05-17 22:55:21 +0800

    add stack

--- src/stack.c
+++ src/stack.c
@@ -1,5 +1,7 @@
 #include "stack.h"
-
+#include "admem.h"
+#include <stddef.h>
+#include <assert.h>
 #define T stack_T
 struct T
 {
@@ -8,23 +10,57 @@ struct T
     {
         void *x;
         struct elem *prev
-    }*head;
+    } *head;
 };
 
-T Stack_new(void){
+T Stack_new(void)
+{
+    T stk;
+    ADNEW(stk);
+    stk->count = 0;
+    stk->head = NULL;
+    return stk;
 }
 
-int Stack_empty(T stk){
+int Stack_empty(T stk)
+{
+    assert(stk);
+    return stk->count == 0;
 }
 
-void* Stack_pop(T stk){
-
+void *Stack_pop(T stk)
+{
+    void *x;
+    struct elem *tmp;
+    assert(stk);
+    assert(stk->count > 0);
+    x = stk->head->x;
+    tmp = stk->head;
+    stk->head = tmp->prev;
+    stk->count--;
+    ADFREE(tmp);
+    return x;
 }
 
-void Stack_push(T stk,void *x){
-
+void Stack_push(T stk, void *x)
+{
+    assert(stk);
+    struct elem *t;
+    ADNEW(t);
+    t->x = x;
+    t->prev = stk->head;
+    stk->head = t;
+    stk->count++;
 }
 
-void Stack_free(T stk){
-    
+void Stack_free(T *stk)
+{
+    struct elem *t,*prev;
+    assert(stk && *stk);
+    for (t = (*stk)->head; t; t = prev)
+    {
+        prev = t->prev;
+        ADFREE(t);
+    }
+    ADFREE(*stk);
 }
